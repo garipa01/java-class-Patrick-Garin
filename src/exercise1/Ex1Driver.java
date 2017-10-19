91cb4543956bd6bcad94853a7e02734d7a5bffe7
garipa01 <garipa01@luther.edu>
2017-10-19 14:22:39 -0500

    Worked on exercise1

--- src/exercise1/Ex1Driver.java
+++ src/exercise1/Ex1Driver.java
@@ -30,31 +30,65 @@ public class Ex1Driver {
    * TODO: Task 1: Print "Hello World" to the default system output
    */
   public static void task1() {
-    throw new UnsupportedOperationException("Task 1 is not implemented yet.");
+    System.out.println("Hello World");
   }
   /**
    * TODO: Task 2: Print all numbers between 1 and 100 (inclusive) replacing all multiples of 3 with "foo", multiples of 5 with "bar", multiples of 3 and 5 with "huh"
    */
   public static void task2() {
-    throw new UnsupportedOperationException("Task 2 is not implemented yet.");
+    for(int i = 1; i <= 100; i++){
+        if(i % 3 == 0 && i % 5 ==0) {
+            System.out.print("Huh ");
+        } 
+        else if(i % 3 ==0){
+            System.out.print("foo ");
+        }
+        else if(i % 5 == 0){
+            System.out.print("bar ");
+        }
+        else{
+            System.out.print(i + " ");
+        }
+    }
+    System.out.println();
   }
   /**
    * TODO: Task 3: Calculate and print the sum of all multiples of 7 between 1 and 100 (inclusive)
    */
   public static void task3() {
-    throw new UnsupportedOperationException("Task 3 is not implemented yet.");
+    int sum = 0;
+    for(int i = 1; i <= 100; i++){
+        if(i % 7 ==0){
+            sum = sum + i;
+        }
+    }
+    System.out.println("" + sum);
   }
   /**
    * TODO: Task 4: Calculate and print sum and product of all numbers between 1 and 20 (inclusive)
    */
   public static void task4() {
-    throw new UnsupportedOperationException("Task 4 is not implemented yet.");
+      int sum = 0;
+      long product = 1;
+      for(int i = 1; i <= 20; i++){
+          sum = sum + i;
+          product = product * i;
+    }
+      System.out.println("Sum of 1-20: " + sum + " Product of 1-20: " + product);
   }
   /**
    * TODO: Task 5: Ask a user to enter their full name and print the initials (ie John Q Doe should yield JQD)
    */
   public static void task5() {
-    throw new UnsupportedOperationException("Task 5 is not implemented yet.");
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please Enter your full name (First Middle Last).");
+    String name = scan.nextLine();
+    String[] list = name.split(" ");
+    String fletter = list[0].substring(0,1);
+    String mletter = list[1].substring(0,1);
+    String lletter = list[2].substring(0,1);
+    System.out.println(fletter + mletter + lletter);
+    
   }
   /**
    * TODO: Task 6: Create an array of 10 elements and initiate each element in it to the cube of its index (Do it in a loop, not during declaration)

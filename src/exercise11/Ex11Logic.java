26c5aedd69e27284cb01be06b4e4fb723287b148
garipa01 <garipa01@luther.edu>
2017-11-30 13:36:05 -0600

    Done with Exercise11

--- src/exercise11/Ex11Logic.java
+++ src/exercise11/Ex11Logic.java
@@ -1,10 +1,12 @@
 package exercise11;
 
+import java.util.ArrayList;
 import java.util.Collection;
+
 
 /**
  * Exercise 11 logic
- * @author yasiro01
+ * @author garipa01 / Patrick Garin
  */
 public class Ex11Logic {
   
@@ -14,6 +16,10 @@ public class Ex11Logic {
    * @param item
    * @return length of the parameter's string representation
    */
+    
+    public <T> int strLen(T item){
+        return item.toString().length();
+    }
   // TODO: Implement method strLen
   
   /**
@@ -25,6 +31,15 @@ public class Ex11Logic {
    * @return the greater of three parameters
    */
   // TODO: Implement method greater
+    public <T extends Comparable> T greater(T item1, T item2, T item3){
+        if(item1.compareTo(item2) >= 0 && item1.compareTo(item3) >= 0){
+            return item1;
+        } else if(item2.compareTo(item3) >= 0) {
+            return item2;
+        } else {
+            return item3;
+        }
+    }
   
   /**
    * Task 3
@@ -33,7 +48,11 @@ public class Ex11Logic {
    * print a list of items
    */
   // TODO: Implement method printList
-  
+  public <T> void printList(T ... list){
+      for(T t: list){
+          System.out.print(t.toString() + " ");
+      }
+  }
   /**
    * Task 4
    * @param <T>
@@ -41,7 +60,23 @@ public class Ex11Logic {
    * @return the smallest item in the collection
    */
   // TODO: Implement method findMinItem
+  public <T extends Comparable> T findMinItem(Collection<T> list){
+      try {
+      T minT = list.iterator().next();
+      for(T t: list){
+          if(t.compareTo(minT) < 0){
+              minT = t;
+          }
+      }
+      return minT;
+      } catch(IndexOutOfBoundsException e){
+          System.out.println("Error");
+          return null;
+      }
+  
+    }
 }
+
 
   /**
    * Task 5
@@ -51,3 +86,37 @@ public class Ex11Logic {
    * Implement the constructor, getters, setters, and toString
    */
   // TODO: Implement class Foo
+
+
+class Foo<T,U> {
+    private T fuzz;
+    private U buzz;
+
+    public Foo(T fuzz, U buzz) {
+        this.fuzz = fuzz;
+        this.buzz = buzz;
+    }
+
+    public T getFuzz() {
+        return fuzz;
+    }
+
+    public void setFuzz(T fuzz) {
+        this.fuzz = fuzz;
+    }
+
+    public U getBuzz() {
+        return buzz;
+    }
+
+    public void setBuzz(U buzz) {
+        this.buzz = buzz;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" + "fuzz=" + fuzz + ", buzz=" + buzz + '}';
+    }
+    
+}
+
